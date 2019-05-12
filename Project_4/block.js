88d88bc4299ba80b91600f0d386763dc8ea988ef
Abhishek Kumar Sinha <aksinha334@gmail.com>
2019-05-12 17:30:31 +0530

    Added block.js

---
+++ Project_4/block.js
@@ -0,0 +1,17 @@
+class Block {
+  constructor(msg) {
+    this.hash = "",
+    this.height = 0,
+    this.body = {
+      this.body.address = "",
+      this.star =
+      {
+        this.body.star.ra = "",
+        this.body.star.dec = "",
+        this.body.star.story = msg,
+      }
+    },
+    this.time = "",
+    this.previousBlockHash = ""
+  }
+}
